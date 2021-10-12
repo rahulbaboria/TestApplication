efbbb464f6488c515dd58069f8c2fd33d040d8b0
rahulbaboria <rahul.b@flick2kniw.com>
2021-10-12 23:21:39 +0530

    initial commit

---
+++ app/src/main/java/com/example/testapplication/application.kt
@@ -0,0 +1,7 @@
+package com.example.testapplication
+
+import android.app.Application
+
+class TestApplication : Application() {
+
+}
---
+++ app/src/main/java/com/example/testapplication/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
